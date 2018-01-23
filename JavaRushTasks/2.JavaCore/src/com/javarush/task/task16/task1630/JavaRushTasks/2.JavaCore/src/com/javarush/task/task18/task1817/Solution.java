6530d38bee09e9b1a379e1493f0a466923179541
exstasy810 <35526998+exstasy810@users.noreply.github.com>
2018-01-23 21:00:03 +0200

    Create Solution.java

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task16/task1630/JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
@@ -0,0 +1,64 @@
+Округление чисел
+
+Считать с консоли 2 имени файла.
+
+Первый файл содержит вещественные(дробные) числа, разделенные пробелом. Например, 3.1415.
+
+Округлить числа до целых и записать через пробел во второй файл.
+
+Закрыть потоки.
+
+Принцип округления:
+
+3.49 - 3
+
+3.50 - 4
+
+3.51 - 4
+
+-3.49 - -3
+
+-3.50 - -3
+
+-3.51 - -4
+
+Требования:
+
+1. Программа должна два раза считать имена файлов с консоли.
+
+2. Для первого файла создай поток для чтения. Для второго - поток для записи.
+
+3. Считать числа из первого файла, округлить их и записать через пробел во второй.
+
+4. Должны соблюдаться принципы округления, указанные в задании.
+
+5. Созданные для файлов потоки должны быть закрыты.
+
+package com.javarush.task.task18.task1820;
+
+/* 
+Округление чисел
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = reader.readLine();
+        String fileName2 = reader.readLine();
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName1));
+        FileWriter writer = new FileWriter(fileName2);
+
+        while (bufferedReader.ready()){
+            String[] s = bufferedReader.readLine().split(" ");
+            for(int i = 0; i < s.length; i++) {
+                writer.write((int) Math.round(Double.parseDouble(s[i])) + " ");
+            }
+        }
+        bufferedReader.close();
+        writer.close();
+        reader.close();
+    }
+}
